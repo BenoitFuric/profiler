3cc36b55259539a747dcb60fe19a2a75a3d977b6
Erik Reyna <erik@erikreyna.com>
2014-11-06 11:02:16 -0500

    apart from the changes that I made for Foreign Policy, which were adding a url in the handlebars template for each person we are profiling, I updated handlebars to v2, changed the references from photoGrid to profilerGrid. Using photoGrid does not provide enough information as there could be other grids on a webpage per se, after changing that references, I changed the references to the object and used the more common "this" form, easier to maintain in this manner should the object naming convention change in the future.

--- js/compiled/display-template.runtime.js
+++ js/compiled/display-template.runtime.js
@@ -1,36 +1,36 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['display-template'] = template(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
-
-
-  buffer += "<div id=\"detailcon\">\n	<div id=\"prevbtn\"><a href=\"javascript:photoGrid.prevPerson()\">«</a></div>\n	<div id=\"closebtn\">X</div>\n	<div id=\"photocon\"><img width=\"115\" height=\"135\" src=\"imgs/";
-  if (helper = helpers.photo) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.photo); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "\"></div>\n	<div style=\"float: left\">\n		<div id=\"name\">";
-  if (helper = helpers.name) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.name); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "</div>\n		<div id=\"data1\" class=\"dataDisp\"><p>Data 1: ";
-  if (helper = helpers.data1) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.data1); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "</p></div>\n		<div id=\"data2\" class=\"dataDisp\"><p>Data 2: ";
-  if (helper = helpers.data2) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.data2); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "</p></div>\n		<div id=\"data3\" class=\"dataDisp\"><p>Data 3: ";
-  if (helper = helpers.data3) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.data3); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "</p></div>\n	</div>\n	<div style=\"clear:both\"></div>\n	<div id=\"blurb\">";
-  if (helper = helpers.blurb) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.blurb); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "</div>\n	<div id=\"nextbtn\"><a href=\"javascript:photoGrid.nextPerson()\">»</a></div>\n</div>";
-  return buffer;
-  });
+templates['display-template'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<div id=\"detailcon\">\n	<div id=\"prevbtn\">\n		<a href=\"javascript:profilerGrid.prevPerson()\">«</a>\n	</div>\n	<div id=\"closebtn\">X</div>\n	<div id=\"photocon\">\n		<img width=\"115\" height=\"135\" src=\"imgs/"
+    + escapeExpression(((helper = (helper = helpers.photo || (depth0 != null ? depth0.photo : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"photo","hash":{},"data":data}) : helper)))
+    + "\">\n	</div>\n	<div style=\"float: left\">\n		<div id=\"name\">"
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "</div>\n		<div id=\"data1\" class=\"dataDisp\">\n			<p>Data 1: <a href=\"https://www.twitter.com/"
+    + escapeExpression(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"url","hash":{},"data":data}) : helper)))
+    + "\">"
+    + escapeExpression(((helper = (helper = helpers.data1 || (depth0 != null ? depth0.data1 : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"data1","hash":{},"data":data}) : helper)))
+    + "</p></a>\n		</div>\n		<div id=\"data2\" class=\"dataDisp\">\n			<p>Data 2: "
+    + escapeExpression(((helper = (helper = helpers.data2 || (depth0 != null ? depth0.data2 : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"data2","hash":{},"data":data}) : helper)))
+    + "</p>\n			</div>\n		<div id=\"data3\" class=\"dataDisp\">\n			<p>Data 3: "
+    + escapeExpression(((helper = (helper = helpers.data3 || (depth0 != null ? depth0.data3 : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"data3","hash":{},"data":data}) : helper)))
+    + "</p>\n		</div>\n	</div>\n	<div style=\"clear:both\"></div>\n	<div id=\"blurb\">"
+    + escapeExpression(((helper = (helper = helpers.blurb || (depth0 != null ? depth0.blurb : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"blurb","hash":{},"data":data}) : helper)))
+    + "</div>\n	<div id=\"nextbtn\"><a href=\"javascript:profilerGrid.nextPerson()\">»</a></div>\n</div>\n";
+},"useData":true});
+templates['th-template'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<div id=\""
+    + escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"id","hash":{},"data":data}) : helper)))
+    + "\" class=\"th\" onclick=\"profilerGrid.showDetail(this.id)\">\n\n	<div id=\"label "
+    + escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"id","hash":{},"data":data}) : helper)))
+    + "\" class=\"thname\">"
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "</div>\n	<div class=\"hoverarea\"></div>\n	<img src=\"imgs/"
+    + escapeExpression(((helper = (helper = helpers.photo || (depth0 != null ? depth0.photo : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"photo","hash":{},"data":data}) : helper)))
+    + "\">\n\n</div>\n";
+},"useData":true});
+templates['th-template.runtime.js'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "(function() {\n  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};\ntemplates['th-template'] = template(function (Handlebars,depth0,helpers,partials,data) {\n  this.compilerInfo = [4,'>= 1.0.0'];\nhelpers = this.merge(helpers, Handlebars.helpers); data = data || {};\n  var buffer = \"\", stack1, helper, functionType=\"function\", escapeExpression=this.escapeExpression;\n\n\n  buffer += \"<div id=\\\"\";\n  if (helper = helpers.id) { stack1 = helper.call(depth0, {hash:{},data:data}); }\n  else { helper = (depth0 && depth0.id); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }\n  buffer += escapeExpression(stack1)\n    + \"\\\" class=\\\"th\\\" onclick=\\\"photoGrid.showDetail(this.id)\\\">\\n\\n	<div id=\\\"label \";\n  if (helper = helpers.id) { stack1 = helper.call(depth0, {hash:{},data:data}); }\n  else { helper = (depth0 && depth0.id); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }\n  buffer += escapeExpression(stack1)\n    + \"\\\" class=\\\"thname\\\">\";\n  if (helper = helpers.name) { stack1 = helper.call(depth0, {hash:{},data:data}); }\n  else { helper = (depth0 && depth0.name); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }\n  buffer += escapeExpression(stack1)\n    + \"</div>\\n	<div class=\\\"hoverarea\\\"></div>\\n	<img src=\\\"imgs/\";\n  if (helper = helpers.photo) { stack1 = helper.call(depth0, {hash:{},data:data}); }\n  else { helper = (depth0 && depth0.photo); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }\n  buffer += escapeExpression(stack1)\n    + \"\\\">\\n\\n</div>\";\n  return buffer;\n  });\n})();";
+  },"useData":true});
 })();
--- js/profiler.js
+++ js/profiler.js
@@ -1,47 +1,48 @@
-var photoGrid = {
+var profilerGrid = {
 
 
 	init: function(args) {
 
 		//INIT GLOBALS
-		photoGrid.initGlobals();
+		this.initGlobals();
 
-		photoGrid.$detail.hide();
-		photoGrid.$prevbtn.hide();
-		
-		photoGrid.loadData(1, args.googleKey);
+		this.$detail.hide();
+		this.$prevbtn.hide();
+
+		this.loadData(1, args.googleKey);
 	},
 
 	initGlobals: function() {
 
 		//GLOBALS
-		photoGrid.current = 0;
-		photoGrid.totalEntries = [];
-		photoGrid.allData = [];
-		photoGrid.ds = [];
+		this.current = 0;
+		this.totalEntries = [];
+		this.allData = [];
+		this.ds = [];
 
 		//JQUERY CACHING
-		photoGrid.$detail = $("#detail");
-		photoGrid.$prevbtn = $("#prevbtn");
-		photoGrid.$nextbtn = $("#nextbtn");
+		this.$detail = $("#detail");
+		this.$prevbtn = $("#prevbtn");
+		this.$nextbtn = $("#nextbtn");
 
 	},
 
 	loadData: function(which, key) {
+		var that = this;
 
 		//LOAD DATA WITH MISO
-		//SAMPLE SPREADSHEET 
+		//SAMPLE SPREADSHEET
 		//https://docs.google.com/spreadsheet/pub?key=0AurS2EUbrPERdE9Nc1dMSGdfenFCN0FCTk9jZF9TVWc&output=html
-		photoGrid.ds = new Miso.Dataset({
+		that.ds = new Miso.Dataset({
 	  		importer : Miso.Dataset.Importers.GoogleSpreadsheet,
 	  		parser : Miso.Dataset.Parsers.GoogleSpreadsheet,
 	  		key : key,
 	  		worksheet : which
 		});
 
-		photoGrid.ds.fetch({ 
+		that.ds.fetch({
 		  success : function() {
-		    photoGrid.populateGrid();
+		    that.populateGrid();
 		  },
 		  error : function() {
 		  	console.error("ERROR: Failed to load Google Spreadsheet");
@@ -52,90 +53,92 @@ var photoGrid = {
 
 	showDetail: function(which) {
 
-		photoGrid.current = which;
+		this.current = which;
 
-		photoGrid.$detail.show();
-		photoGrid.$detail.animate({ opacity: '1' }, 500);
+		this.$detail.show();
+		this.$detail.animate({ opacity: '1' }, 500);
 
 		var context = {
-			name: photoGrid.allData[which][0].name,
-			photo: photoGrid.allData[which][0].photourl,
-			blurb: photoGrid.allData[which][0].blurb,
-			data1: photoGrid.allData[which][0].data1,
-			data2: photoGrid.allData[which][0].data2,
-			data3: photoGrid.allData[which][0].data3
+			name: this.allData[which][0].name,
+			photo: this.allData[which][0].photourl,
+			blurb: this.allData[which][0].blurb,
+			url: this.allData[which][0].url,
+			data1: this.allData[which][0].data1,
+			data2: this.allData[which][0].data2,
+			data3: this.allData[which][0].data3
 		}
 
-		photoGrid.$detail.html(Handlebars.templates['display-template'](context));
+		this.$detail.html(Handlebars.templates['display-template'](context));
 
-		photoGrid.checkNav();
+		this.checkNav();
 
 		//CLOSE BUTTON
 		$("#closebtn").click(function() {
-			photoGrid.$detail.hide();
-			photoGrid.$detail.animate({ opacity: '0' }, 500);
+			this.$detail.hide();
+			this.$detail.animate({ opacity: '0' }, 500);
 			$("#detail #vid").empty();
 		});
 
 	},
 
 	nextPerson: function() {
-		photoGrid.current ++;
-		photoGrid.showDetail(photoGrid.current);
+		this.current ++;
+		this.showDetail(this.current);
 	},
 
 	prevPerson: function() {
-		photoGrid.current --;
-		photoGrid.showDetail(photoGrid.current);
+		this.current --;
+		this.showDetail(this.current);
 	},
 
 	checkNav: function() {
 
-		if (photoGrid.current == 0) {
-			//photoGrid.$prevbtn.hide();
+		if (this.current == 0) {
+			//this.$prevbtn.hide();
 			$("#prevbtn").hide();
 		} else {
-			//photoGrid.$prevbtn.show();
+			//this.$prevbtn.show();
 			$("#prevbtn").show();
 		}
 
-		if (photoGrid.current == photoGrid.totalEntries - 1) {
-			//photoGrid.$nextbtn.hide();
+		if (this.current == this.totalEntries - 1) {
+			//this.$nextbtn.hide();
 			$("#nextbtn").hide();
 		} else {
-			//photoGrid.$nextbtn.show();
+			//this.$nextbtn.show();
 			$("#nextbtn").show();
 		}
 	},
 
 	populateGrid: function() {
 
-		var $len = photoGrid.ds.column("name").data.length;
-		photoGrid.totalEntries = $len;
-		
+		var $len = this.ds.column("name").data.length;
+		this.totalEntries = $len;
+
 		//LOOP THRU GOOGLE DATA AND PUT INTO OBJECT
 		for (var j=0; j<$len; j++) {
-			var counter = photoGrid.ds.column("id").data[j];
-			photoGrid.allData[counter] = [ {
-									myid: photoGrid.ds.column("id").data[j],
-									name: photoGrid.ds.column("name").data[j],
-									data1: photoGrid.ds.column("data1").data[j],
-									data2: photoGrid.ds.column("data2").data[j],
-									data3: photoGrid.ds.column("data3").data[j],
-									blurb: photoGrid.ds.column("blurb").data[j],
-									photourl: photoGrid.ds.column("image").data[j],
-							    }];
+			var counter = this.ds.column("id").data[j];
+			this.allData[counter] = [ {
+				myid: this.ds.column("id").data[j],
+				name: this.ds.column("name").data[j],
+				url: this.ds.column("url").data[j],
+				data1: this.ds.column("data1").data[j],
+				data2: this.ds.column("data2").data[j],
+				data3: this.ds.column("data3").data[j],
+				blurb: this.ds.column("blurb").data[j],
+				photourl: this.ds.column("image").data[j]
+		    }];
 		}
-		
-		for (var i = 0; i<photoGrid.totalEntries; i++) {
+
+		for (var i = 0; i<this.totalEntries; i++) {
 
 			var context = {
 				id: i,
-				name: photoGrid.allData[i][0].name, photo:
-				photoGrid.allData[i][0].photourl
+				name: this.allData[i][0].name,
+				photo: this.allData[i][0].photourl
 			}
 
-			$("#container").append(Handlebars.templates['th-template'](context));	
+			$("#container").append(Handlebars.templates['th-template'](context));
 		}
 
 	}
